8c31b0fb42dc8d3a7b74915bc1d6ae596daa76c5
Hiroshi Fujiwara <fujiwara@no1s.biz>
2024-08-10 06:59:00 +0000

    fix chapter02

---
+++ chapter02/src/templates/app/hello/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div className="main">
+      <h1 className="p-4 bg-rose-200 text-xl">Hello</h1>
+      <div className="p-4">Hello, This is NextJS</div>
+    </div>
+  );
+}
