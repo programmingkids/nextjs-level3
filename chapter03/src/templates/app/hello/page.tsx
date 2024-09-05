b1f5a9654e6d172f27a9478f1f26f6092eb5d779
Hiroshi Fujiwara <fujiwara@no1s.biz>
2024-09-05 12:04:48 +0000

    fix chapter03 work02 read to red

--- chapter03/src/templates/app/hello/page.tsx
+++ chapter03/src/templates/app/hello/page.tsx
@@ -1,24 +1,34 @@
 'use client';
 
-import { useRouter } from "next/navigation";
+import { useRouter } from 'next/navigation';
 
 export default function Page() {
   const router = useRouter();
 
-  const handleClickBlue = (e:React.MouseEvent<HTMLButtonElement>) => {
+  const handleClickBlue = (e: React.MouseEvent<HTMLButtonElement>) => {
     router.push('/color/blue');
-  }
+  };
 
-  const handleClickRed = (e:React.MouseEvent<HTMLButtonElement>) => {
+  const handleClickRed = (e: React.MouseEvent<HTMLButtonElement>) => {
     router.replace('/color/red');
-  }
-  
+  };
+
   return (
     <div className="main">
       <h1 className="p-4 bg-blue-200 text-xl">Hello</h1>
       <div className="py-4 px-6 flex justify-center gap-4">
-        <button onClick={handleClickBlue} className="bg-blue-500 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded">Blue</button>
-        <button onClick={handleClickRed} className="bg-rose-500 hover:bg-rose-800 text-white font-bold py-2 px-4 rounded">Read</button>
+        <button
+          onClick={handleClickBlue}
+          className="bg-blue-500 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded"
+        >
+          Blue
+        </button>
+        <button
+          onClick={handleClickRed}
+          className="bg-rose-500 hover:bg-rose-800 text-white font-bold py-2 px-4 rounded"
+        >
+          Red
+        </button>
       </div>
     </div>
   );
